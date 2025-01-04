792c75cf89ac4a531041241e07849c5556a169c7
Syed Taha <syetaha@gmail.com>
2025-01-04 23:07:44 +0500

    Add conversion methods to FlatConverter for FlatVector from sf::Vector2f and sf::Vector2i

--- FlatPhysics/headers/FlatConverter.hpp
+++ FlatPhysics/headers/FlatConverter.hpp
@@ -14,18 +14,26 @@ namespace FlatPhysics {
     class FlatConverter {
     public:
         // Convert FlatVector to sf::Vector2f
-        static inline sf::Vector2f toVector2f(const FlatPhysics::FlatVector& v) {
+        static inline sf::Vector2f toVector2f(const FlatVector& v) {
             return sf::Vector2f(v.x, v.y);
         }
 
         // Convert to sf::Vector2f array
-        static inline void toVector2fArray(const std::vector<FlatPhysics::FlatVector>& vertices,
+        static inline void toVector2fArray(const std::vector<FlatVector>& vertices,
             std::vector<sf::Vector2f>& sfVertices)
         {
             sfVertices.resize(vertices.size());
             for (int i = 0; i < vertices.size(); i++) {
                 sfVertices[i] = sf::Vector2f(vertices[i].x, vertices[i].y);
             }
+        }
+
+        static inline FlatVector toFlatVector(const sf::Vector2f& v) {
+            return FlatVector(v.x, v.y);
+        }
+
+        static inline FlatVector toFlatVector(const sf::Vector2i& v) {
+            return FlatVector(v.x, v.y);
         }
 
     };
