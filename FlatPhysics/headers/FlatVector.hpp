677efbed709864c6fcc72b4831ee6e7bd055968a
Syed Taha <syetaha@gmail.com>
2025-01-04 23:06:52 +0500

    Enhance FlatVector class with in-place addition and subtraction operators, and add scalar multiplication support

--- FlatPhysics/headers/FlatVector.hpp
+++ FlatPhysics/headers/FlatVector.hpp
@@ -22,10 +22,12 @@ namespace FlatPhysics {
         FlatVector(float x = 0.0f, float y = 0.0f);
 
         FlatVector operator+(const FlatVector& other) const; // Addition
-        FlatVector& operator+=(const FlatVector& other); // Addition
+        FlatVector& operator+=(const FlatVector& other); // In-place addition
         FlatVector operator-(const FlatVector& other) const; // Subtraction
+        FlatVector& operator-=(const FlatVector& other); // In-place subtraction
         FlatVector operator-() const; // Negation
         FlatVector operator*(float scalar) const; // Multiplication
+        friend FlatVector operator*(float scalar, const FlatVector& vector); // Multiplication
         FlatVector operator/(float scalar) const; // Division
         bool operator==(const FlatVector& other) const; // Check for equality
         std::string toString() const; // To string
--- FlatPhysics/src/FlatVector.cpp
+++ FlatPhysics/src/FlatVector.cpp
@@ -6,49 +6,64 @@ using std::to_string;
 #include "../headers/FlatVector.hpp"
 #include "../headers/FlatTransformation.hpp"
 
-// Define the zero vector
-FlatPhysics::FlatVector FlatPhysics::FlatVector::Zero = FlatVector(0, 0);
+namespace FlatPhysics {
+    // Define the zero vector
+    FlatVector FlatVector::Zero = FlatVector(0, 0);
 
 
-FlatPhysics::FlatVector::FlatVector(float x, float y) : x(x), y(y) {}
+    FlatVector::FlatVector(float x, float y) : x(x), y(y) {}
 
-FlatPhysics::FlatVector FlatPhysics::FlatVector::operator+(const FlatVector& other) const {
-    return FlatVector(x + other.x, y + other.y);
-}
+    FlatVector FlatVector::operator+(const FlatVector& other) const {
+        return FlatVector(x + other.x, y + other.y);
+    }
 
-FlatPhysics::FlatVector& FlatPhysics::FlatVector::operator+=(const FlatVector& other) {
-    x += other.x;
-    y += other.y;
-    return *this;
-}
+    FlatVector& FlatVector::operator+=(const FlatVector& other) {
+        x += other.x;
+        y += other.y;
+        return *this;
+    }
 
-FlatPhysics::FlatVector FlatPhysics::FlatVector::operator-(const FlatVector& other) const {
-    return FlatVector(x - other.x, y - other.y);
-}
+    FlatVector FlatVector::operator-(const FlatVector& other) const {
+        return FlatVector(x - other.x, y - other.y);
+    }
 
-FlatPhysics::FlatVector FlatPhysics::FlatVector::operator-() const {
-    return FlatVector(-x, -y);
-}
+    FlatVector& FlatVector::operator-=(const FlatVector& other) {
+        x -= other.x;
+        y -= other.y;
+        return *this;
+    }
 
-FlatPhysics::FlatVector FlatPhysics::FlatVector::operator*(float scalar) const {
-    return FlatVector(x * scalar, y * scalar);
-}
 
-FlatPhysics::FlatVector FlatPhysics::FlatVector::operator/(float scalar) const {
-    return FlatVector(x / scalar, y / scalar);
-}
+    FlatVector FlatVector::operator-() const {
+        return FlatVector(-x, -y);
+    }
 
-bool FlatPhysics::FlatVector::operator==(const FlatVector& other) const {
-    return x == other.x && y == other.y;
-}
+    FlatVector FlatVector::operator*(float scalar) const {
+        return FlatVector(x * scalar, y * scalar);
+    }
 
-std::string FlatPhysics::FlatVector::toString() const {
-    return "(" + std::to_string(x) + ", " + std::to_string(y) + ")";
-}
 
-FlatPhysics::FlatVector FlatPhysics::FlatVector::Transform(const FlatVector& vector, FlatTransformation& transform) {
-    return FlatVector(
-        vector.x * transform.cosine - vector.y * transform.sine + transform.PositionX,
-        vector.x * transform.sine + vector.y * transform.cosine + transform.PositionY
-    );
-}
+    FlatVector operator*(float scalar, const FlatVector& vector) {
+        return FlatVector(vector.x * scalar, vector.y * scalar);
+    }
+
+
+    FlatVector FlatVector::operator/(float scalar) const {
+        return FlatVector(x / scalar, y / scalar);
+    }
+
+    bool FlatVector::operator==(const FlatVector& other) const {
+        return x == other.x && y == other.y;
+    }
+
+    std::string FlatVector::toString() const {
+        return "(" + std::to_string(x) + ", " + std::to_string(y) + ")";
+    }
+
+    FlatVector FlatVector::Transform(const FlatVector& vector, FlatTransformation& transform) {
+        return FlatVector(
+            vector.x * transform.cosine - vector.y * transform.sine + transform.PositionX,
+            vector.x * transform.sine + vector.y * transform.cosine + transform.PositionY
+        );
+    }
+} // namespace FlatPhysics
