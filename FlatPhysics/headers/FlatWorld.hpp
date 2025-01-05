6aa21f68b41a78306769a6f69239750b47f02c8a
Syed Taha <syetaha@gmail.com>
2025-01-05 19:53:00 +0500

    Enhance FlatWorld class with collision handling improvements and stopwatch integration

--- FlatPhysics/headers/FlatWorld.hpp
+++ FlatPhysics/headers/FlatWorld.hpp
@@ -8,16 +8,22 @@
 #define FLAT_WORLD_HPP
 
 #include <vector>
+#include "../../FlatUtils/StopWatch.hpp"
+#include "FlatManifold.hpp"
+
 
 namespace FlatPhysics {
 
-    // Forward Declaration of FlatBody and FlatVector
+    // Forward Declaration of FlatBody, FlatVector, and FlatManifold
     class FlatBody;
     struct FlatVector;
+    struct FlatManifold;
+
 
 
     class FlatWorld {
         std::vector<FlatBody*> bodies;
+        std::vector<FlatManifold> contactList;
 
     public:
         // Define Constants
@@ -27,6 +33,11 @@ namespace FlatPhysics {
 
         static constexpr float MinDensity = 0.5f; // gm/cm^3
         static constexpr float MaxDensity = 21.45f; // gm/cm^3
+
+        static constexpr size_t MinIterations = 1;
+        static constexpr size_t MaxIterations = 128;
+
+        static FlatUtils::Stopwatch worldStopwatch;
 
 
         // Constructor
@@ -39,9 +50,9 @@ namespace FlatPhysics {
         void removeBody(int index);
         bool getBody(int index, FlatBody*& body);
 
-        void step(float time);
+        void step(float time, size_t iterations = MinIterations);
 
-        void resolveCollisions(FlatBody*& bodyA, FlatBody*& bodyB, FlatVector& normal, float depth);
+        void resolveCollisions(const FlatManifold& collisionManifold);
 
         bool collides(FlatBody*& bodyA, FlatBody*& bodyB, FlatVector& normal, float& depth);
 
--- FlatPhysics/src/FlatWorld.cpp
+++ FlatPhysics/src/FlatWorld.cpp
@@ -10,6 +10,7 @@
 
 
 FlatPhysics::FlatVector FlatPhysics::FlatWorld::Gravity = FlatPhysics::FlatVector(0.0f, -9.8f);
+FlatUtils::Stopwatch FlatPhysics::FlatWorld::worldStopwatch = FlatUtils::Stopwatch();
 
 FlatPhysics::FlatWorld::~FlatWorld() {
     for (size_t i = 0; i < bodies.size(); i++) {
@@ -22,12 +23,15 @@ void FlatPhysics::FlatWorld::addBody(FlatBody* body) {
 }
 
 void FlatPhysics::FlatWorld::removeBody(int index) {
-    if (index >= 0 && index < bodies.size()) {
-        bodies.erase(bodies.begin() + index);
-    }
-    else {
+    if (index < 0 || index >= bodies.size()) {
         throw std::out_of_range("Index out of bounds.");
     }
+
+    // Delete the body
+    delete bodies[index];
+
+    // Remove the body from the list of bodies
+    bodies.erase(bodies.begin() + index);
 }
 
 bool FlatPhysics::FlatWorld::getBody(int index, FlatBody*& body) {
@@ -38,61 +42,101 @@ bool FlatPhysics::FlatWorld::getBody(int index, FlatBody*& body) {
     return false;
 }
 
-void FlatPhysics::FlatWorld::step(float time) {
+void FlatPhysics::FlatWorld::step(float time, size_t iterations) {
+    iterations = FlatMath::Clamp<size_t>(iterations, MinIterations, MaxIterations);
 
-    // Move
-    for (size_t i = 0; i < bodies.size(); i++) {
-        bodies[i]->step(time, FlatWorld::Gravity);
-    }
+    // Initilize normal and depth, only once instead of in each loop
+    FlatPhysics::FlatVector normal = FlatPhysics::FlatVector::Zero;
+    float depth = 0.0f;
 
-    // Collide
-    for (size_t i = 0; i < bodies.size() - 1; i++) {
-        FlatPhysics::FlatBody* bodyA = bodies[i];
+    // Make pointers to the bodies, once instead of in each loop
+    FlatPhysics::FlatBody* bodyA = nullptr;
+    FlatPhysics::FlatBody* bodyB = nullptr;
 
-        for (size_t j = i + 1; j < bodies.size(); j++) {
-            FlatPhysics::FlatBody* bodyB = bodies[j];
-            FlatPhysics::FlatVector normal;
-            float depth;
 
-            // Check if both bodies are static
-            if (bodyA->isStatic && bodyB->isStatic) {
-                continue;
-            }
+    for (size_t it = 0; it < iterations; it++) {
+        // Move
+        for (size_t i = 0; i < bodies.size(); i++) {
+            bodies[i]->step(time / iterations, Gravity);
+        }
 
-            if (collides(bodyA, bodyB, normal, depth)) {
-                if (bodyA->isStatic) bodyB->move(normal * depth); // If body A is static, move body B
-                else if (bodyB->isStatic) bodyA->move(-normal * depth);// If body B is static, move body A
-                else { // If both are dynamic, move both
-                    bodyA->move(-normal * (depth / 2.0f));
-                    bodyB->move(normal * (depth / 2.0f));
+        // Clear the contact list
+        contactList.clear();
+
+        // Collide
+        FlatWorld::worldStopwatch.start();
+        for (size_t i = 0; i < bodies.size() - 1; i++) {
+            bodyA = bodies[i];
+
+            for (size_t j = i + 1; j < bodies.size(); j++) {
+                bodyB = bodies[j];
+
+                // Check if both bodies are static
+                if (bodyA->isStatic && bodyB->isStatic) continue;
+
+                if (collides(bodyA, bodyB, normal, depth)) {
+                    if (bodyA->isStatic) bodyB->move(normal * depth); // If body A is static, move body B
+                    else if (bodyB->isStatic) bodyA->move(-normal * depth);// If body B is static, move body A
+                    else { // If both are dynamic, move both
+                        bodyA->move(-normal * (depth / 2.0f));
+                        bodyB->move(normal * (depth / 2.0f));
+                    }
+
+                    contactList.emplace_back(bodyA, bodyB, depth, normal, FlatVector::Zero, FlatVector::Zero, 0);
+
                 }
 
-                // Resolve the collision
-                resolveCollisions(bodyA, bodyB, normal, depth);
             }
-
         }
+
+        // Resolve Collisions
+        for (size_t i = 0; i < contactList.size(); i++) {
+            resolveCollisions(contactList[i]);
+        }
+
+        FlatWorld::worldStopwatch.stop();
     }
 }
 
-void FlatPhysics::FlatWorld::resolveCollisions(FlatBody*& bodyA, FlatBody*& bodyB,
-    FlatVector& normal, float depth)
+void FlatPhysics::FlatWorld::resolveCollisions(const FlatManifold& collisionManifold)
 {
-    // Calculate Relative velocity
-    FlatVector relativeVelocity = bodyB->linearVelocity - bodyA->linearVelocity;
 
-    if (FlatMath::DotProduct(relativeVelocity, normal) > 0) {
-        return;
-    }
+    FlatBody* bodyA = collisionManifold.bodyA;
+    FlatBody* bodyB = collisionManifold.bodyB;
+    FlatVector normal = collisionManifold.normal;
+    float depth = collisionManifold.depth;
+
+
+    // Relative velocity = velocity of B - velocity of A
+    // Get the dot product of relative velocity and normal
+    float dotProduct = FlatMath::DotProduct(bodyB->linearVelocity - bodyA->linearVelocity, normal);
+
+    // If the dot product is greater than 0, the bodies are moving away from each other
+    // No need to resolve the collision
+    if (dotProduct > 0) return;
 
     // Calculate restitution
     float e = std::min(bodyA->restitution, bodyB->restitution);
 
-    // Calculate Impulse magnitude
-    float impulseMagnitude = -(1 + e) * FlatMath::DotProduct(relativeVelocity, normal);
-    impulseMagnitude /= (bodyA->inverseMass + bodyB->inverseMass);
+    /*
+                            - (1 + e) * RelativeVelocity ∙ Normal
+        Impulse Magnitude = ------------------------------------
+                                    1 / massA + 1 / massB
 
-    FlatVector impulse = impulseMagnitude * normal;
+        Simplified to:
+        dotProduct = RelativeVelocity ∙ Normal
+
+                                 - (1 + e) * dotProduct
+        Impulse Magnitude = -------------------------------
+                              InverseMassA + InverseMassB
+
+        And then the Impulse
+        Impulse = Impulse Magnitude * Normal
+    */
+
+    // Calculate Impulse 
+    FlatVector impulse = -(1 + e) * dotProduct * normal;
+    impulse /= (bodyA->inverseMass + bodyB->inverseMass);
 
     bodyA->linearVelocity -= impulse * bodyA->inverseMass;
     bodyB->linearVelocity += impulse * bodyB->inverseMass;
@@ -101,8 +145,6 @@ void FlatPhysics::FlatWorld::resolveCollisions(FlatBody*& bodyA, FlatBody*& body
 bool FlatPhysics::FlatWorld::collides(FlatBody*& bodyA, FlatBody*& bodyB,
     FlatVector& normal, float& depth)
 {
-    normal = FlatVector::Zero;
-    depth = 0.0f;
 
     ShapeType shapeA = bodyA->shapeType;
     ShapeType shapeB = bodyB->shapeType;
