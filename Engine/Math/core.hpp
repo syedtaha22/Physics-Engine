8bfc2289c556a5da99af11f986cedc8ed086baae
Syed Taha <syetaha@gmail.com>
2025-01-11 23:13:00 +0500

    Add Math core header file to include essential math components

---
+++ Engine/Math/core.hpp
@@ -0,0 +1,12 @@
+#ifndef MATH_CORE_HPP
+#define MATH_CORE_HPP
+
+// Include Math core files
+#include "headers/Vector.hpp"
+#include "headers/Transformation.hpp"
+#include "headers/BoundingBox.hpp"
+#include "headers/Operation.hpp"
+#include "headers/Converter.hpp"
+
+
+#endif // MATH_CORE_HPP
