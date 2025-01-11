93224c1d39c5e141efdfa07e2661d0cb66766d56
Syed Taha <syetaha@gmail.com>
2025-01-11 23:09:54 +0500

    Rename FlatMath to Operation and move it to Engine/Math

---
+++ Engine/Math/headers/Operation.hpp
@@ -0,0 +1,108 @@
+/*
+
+    Contains all the math functions and classes used in the physics engine.
+
+*/
+
+#ifndef OPERATION_HPP
+#define OPERATION_HPP
+
+#include <vector>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include "Vector.hpp"
+
+namespace Math {
+
+
+    class Operation {
+
+    public:
+
+        static constexpr float FloatMax = 3.4028235e+38F; // Maximum value of a float
+        static constexpr float FloatMin = -3.4028235e+38F; // Minimum value of a float
+
+        // Dot Product
+        static inline float DotProduct(const Vector& v1, const Vector& v2) {
+            return v1.x * v2.x + v1.y * v2.y;
+        }
+
+        // Cross Product
+        static inline float CrossProduct(const Vector& v1, const Vector& v2) {
+            return v1.x * v2.y - v1.y * v2.x;
+        }
+
+        // Lenght of a vector
+        static inline float Length(const Vector& v) {
+            return sqrt(v.x * v.x + v.y * v.y);
+        }
+
+        // Normalize a vector
+        static inline Vector Normalize(const Vector& v) {
+            float len = Length(v);
+            return Vector(v.x / len, v.y / len);
+        }
+
+        // Distance between two points
+        static inline float Distance(const Vector& v1, const Vector& v2) {
+            return Length(v1 - v2);
+        }
+
+        // Squared Distance between two points
+        static inline float SquaredDistance(const Vector& v1, const Vector& v2) {
+            return (v1.x - v2.x) * (v1.x - v2.x) + (v1.y - v2.y) * (v1.y - v2.y);
+        }
+
+        // Clamp
+        template <typename T>
+        static T Clamp(T value, T min, T max) {
+            if (value < min) return min;
+            if (value > max) return max;
+            return value;
+        }
+
+        static Vector ArtimeticMean(const std::vector<Vector>& vertices);
+
+        // Edge between two points
+        static inline Vector EdgeBetween(const Vector& p1, const Vector& p2) {
+            /*
+                Edge from point 1 to point 2:
+                Edge = Point 2 - Point 1
+            */
+            return p2 - p1;
+        }
+
+        // Round a number
+        static inline float Round(float number, size_t decimalPlaces) {
+            float factor = static_cast<float>(std::pow(10, decimalPlaces));
+            return std::round(std::trunc(number * factor)) / factor;
+        }
+
+        // Round a number and return as a string
+        static inline std::string RoundToString(double number, size_t decimalPlaces) {
+            std::ostringstream oss;
+            oss << std::fixed << std::setprecision(decimalPlaces) << number;
+            return oss.str();
+        }
+
+
+        static inline bool NearlyEqual(float a, float b, float epsilon) {
+            return std::abs(a - b) < epsilon;
+        }
+
+        // Check if two vectors are nearly equal
+        static inline bool NearlyEqual(const Vector& v1, const Vector& v2, float epsilon) {
+            return SquaredDistance(v1, v2) < epsilon * epsilon;
+        }
+
+        // Get the perpendicular vector
+        static inline Vector Perpendicular(const Vector& v) {
+            return Vector(-v.y, v.x);
+        }
+
+    };
+} // namespace Math
+
+#endif // OPERATION_HPP
---
+++ Engine/Math/src/Operation.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+
+using std::sqrt;
+
+#include "../headers/Operation.hpp"
+#include "../headers/Vector.hpp"
+
+
+namespace Math {
+    Vector Operation::ArtimeticMean(const std::vector<Vector>& vertices) {
+        float sumX = 0.0f;
+        float sumY = 0.0f;
+        for (int i = 0; i < vertices.size(); i++) {
+            sumX += vertices[i].x;
+            sumY += vertices[i].y;
+        }
+        return Vector(sumX / vertices.size(), sumY / vertices.size());
+    }
+} // namespace Math
