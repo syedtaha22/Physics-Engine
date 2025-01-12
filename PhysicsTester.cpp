3079d4def0ba7dbdc94e2d4033cf2e0dc71e7acf
Syed Taha <syetaha@gmail.com>
2025-01-12 21:10:09 +0500

    Refactor PhysicsTester to use Physics::World and add functionality for adding bodies in orbits

--- PhysicsTester.cpp
+++ PhysicsTester.cpp
@@ -3,6 +3,13 @@
 #include <memory>
 #include <sstream>
 #include <iomanip>
+#include <unordered_map>
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+
+const double G = 6.67430e-11; // Gravitational constant
 
 // Include Flat Core
 #include "Flat/core.hpp"
@@ -14,29 +21,29 @@
 #include "Engine/Math/core.hpp"
 
 #include "Utils/StopWatch.hpp" // Include stopwatch utility
+#include "Utils/Random.hpp" // Include random utility
 
 
-static constexpr float SunMass = 1.989e30f;
-static constexpr float EarthMass = 5.972e24f;
-
-static constexpr float SunRadius = 6.9634e8f; // meters
-static constexpr float EarthRadius = 6.378e6f; // meters
-
-static constexpr float EarthDistance = 147.12e9f;
-
+constexpr double RADIUS = 1e10; // Radius of the circle
+constexpr double MASS = 5e23; // Mass of each body
 
 class Game {
     Flat::Window window;
 
-    std::vector<std::shared_ptr<Physics::Body>> bodies;
+    Physics::World world;
 
     std::vector<Flat::Color> colors;
 
     std::string scaleText;
 
+    bool started = false;
+    bool showGrid = true;
+
+    std::vector<Physics::Path> orbits;
+
 public:
     Game() : window(1280, 768, "Physics Engine from Scratch", Flat::Color::Black, 5.0f, 32) {
-        // Use ostringstream to convert the float to a string in scientific notation
+        // Use ostringstream to convert the double to a string in scientific notation
         std::ostringstream oss;
         oss << std::scientific << std::setprecision(0) << Math::Converter::getScale();
 
@@ -44,13 +51,36 @@ public:
 
 
         // Add a Sun-Like Body
-        bodies.push_back(Physics::Body::CreateCircularBody(SunRadius, SunMass, Math::Vector(0, 0)));
+        world.addBody(Physics::Body::AstronomicalBody(5e26, Math::Vector(0, 0)));
         colors.push_back(Flat::Color::Yellow);
 
         // Add an Earth-Like Body
-        bodies.push_back(Physics::Body::CreateCircularBody(EarthRadius, EarthMass, Math::Vector(0, EarthDistance)));
-        colors.push_back(Flat::Color::Blue);
+        add_bodies_around_circle(4, RADIUS, MASS);
+        orbits.resize(world.numBodies());
+    }
 
+    // Function to add bodies around a circle
+    void add_bodies_around_circle(int n, double radius, double mass) {
+        // Get the mass of the central body
+        double central_mass = world.getBody(0)->getPhysicalProperty("Mass");
+
+        // Calculate the escape velocity just once
+        double escape_velocity = sqrt(2 * G * central_mass / radius);
+
+        for (int i = 0; i < n; ++i) {
+            double angle = 2 * M_PI * i / n; // Angle for equidistant placement
+            double x = radius * cos(angle); // X position
+            double y = radius * sin(angle); // Y position
+
+            double vx = -sin(angle) * (sqrt(G * 5e24 / radius)) * 5; // Tangential velocity (Vx)
+            double vy = cos(angle) * (sqrt(G * 5e24 / radius)) * 5; // Tangential velocity (Vy)
+
+            std::shared_ptr<Physics::Body> body = Physics::Body::AstronomicalBody(mass, Math::Vector(x, y));
+            body->setKinematicProperty("LinearVelocity", Math::Vector(vx, vy));
+
+            world.addBody(body); // Add body to the world
+            colors.push_back(Utils::Random::getRandomColor()); // Add a random color
+        }
     }
 
     void update() {
@@ -60,19 +90,31 @@ public:
                 window.close();
 
             // Check for zoom
-            if (event.type == (int)Flat::Event::MouseWheelScrolled) {
+            if (event.type == sf::Event::MouseWheelScrolled) {
                 if (event.mouseWheelScroll.delta > 0)
                     window.zoom(0.95f); // Zoom in
                 else
                     window.zoom(1.05f); // Zoom out
             }
 
+            // Check for Start/Stop
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Space) {
+                    started = !started;
+                }
 
+                // Check for Grid
+                if (event.key.code == sf::Keyboard::G) {
+                    showGrid = !showGrid;
+                }
+            }
         }
 
 
         // stopwatch.start();
-        // world.step(window.getElapsedTimeSinceLastFrame(Flat::TimeUnit::Seconds), 20);
+        if (started) {
+            world.step(window.getElapsedTimeSinceLastFrame(Flat::TimeUnit::Seconds) * 1e6);
+        }
         // // stopwatch.stop();
 
         // this->totalStepTime += stopwatch.getElapsedTimeInMilliseconds();
@@ -110,45 +152,68 @@ public:
 
     void draw() {
         window.clear();
-        window.drawGridLines(1.0f, Flat::Color("#333333"));
+
+        if (showGrid)
+            window.drawGridLines(1.0f, Flat::Color("#333333"));
 
         // Draw the bodies
-        for (int i = 0; i < bodies.size(); i++) {
-            auto body = bodies[i];
+        for (int i = 0; i < world.numBodies(); i++) {
+            std::shared_ptr<Physics::Body> body = world.getBody(i);
+
+            if (!body) {
+                throw std::runtime_error("Error getting body at index " + std::to_string(i));
+            }
 
             // Get the position
             Math::Vector pos = body->getKinematicProperty("Position");
-            float scale = Math::Converter::getScale();
 
-            // Draw the body
-            if (!body->physicalPropertyExists("Radius")) {
-                std::cout << "Body does not have a radius" << std::endl;
-                continue;
-            }
+            orbits[i].insert(pos);
 
-            window.drawCircleWithBorder(body->getPhysicalProperty("Radius") / scale,
-                Math::Converter::toVector2f(pos), colors[i], 0.1f, colors[i]);
+            window.drawCircleWithBorder(0.01f, Math::Converter::toVector2f(pos), colors[i], 0.1f, colors[i]);
 
         }
+
+        DrawOrbits();
 
         displayScale();
 
         window.display();
     }
 
+    void DrawOrbits() {
+        if (orbits.size() < 2) return;
+
+        for (int i = 0; i < orbits.size(); i++) {
+            for (int j = 0; j < orbits[i].getSize() - 1; j++) {
+
+                window.drawLine(
+                    Math::Converter::toVector2f(orbits[i].get(j)),
+                    Math::Converter::toVector2f(orbits[i].get(j + 1)),
+                    colors[i].withAlha(80));
+            }
+        }
+    }
+
+
     void displayScale() {
-
-
-        // Place the text on the bottom left corner
-        // Get the left and bottom of the camera
         float left = window.getCameraLeft();
+        float top = window.getCameraTop();
+        float padding = abs(top - window.getCameraBottom()) * 0.05f;
+
+        if (!started) {
+            float top = window.getCameraTop();
+            window.writeText("Paused", sf::Vector2f(left + padding, top - padding),
+                Flat::Color::White, 1, false, 0.7f);
+
+        }
+
+
+
         float bottom = window.getCameraBottom();
-        float padding = abs(window.getCameraRight() - left) * 0.03f;
 
 
         window.writeText(scaleText, sf::Vector2f(left + padding, bottom + padding),
-            Flat::Color::White, 1, false);
-
+            Flat::Color::White, 1, false, 0.7f);
 
     }
 
@@ -159,7 +224,7 @@ public:
 };
 
 int main() {
-    Math::Converter::setScale(1e10f);
+    Math::Converter::setScale(1e9f);
 
     Game game;
 
