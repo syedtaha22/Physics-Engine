ec321b19a157e28999b3cb89395581fd157eafaa
Syed Taha <syetaha@gmail.com>
2025-01-13 21:29:35 +0500

    Refactor World class: replace gravitational force calculation with direct acceleration computation for bodies

--- Engine/Physics/src/World.cpp
+++ Engine/Physics/src/World.cpp
@@ -33,69 +33,52 @@ namespace Physics {
     }
 
     void World::step(double time) {
-        calculateGForces();
+        calculateBodyAccelerations();
         for (size_t i = 0; i < bodies.size(); i++) {
             bodies[i]->step(time);
         }
     }
 
-    void World::calculateGForces() {
-        // Loop over each body, and calculate the gravitational forces
-        for (size_t i = 0; i < bodies.size() - 1; i++) {
-            for (size_t j = i + 1; j < bodies.size(); j++) {
-                calculateGravitionalForce(bodies[i], bodies[j]);
-            }
-        }
-    }
-
-    void World::calculateGravitionalForce(std::shared_ptr<Body> body1, std::shared_ptr<Body> body2) {
-
+    void World::calculateBodyAccelerations() {
         /*
-            Newton's Law of Universal Gravitation:
-            F_Mag = G * (m1 * m2) / r^2
+            Calculate the Gravitational Acceleration acting on each body
 
-            F_Dir = F_Mag * (r2 - r1) / r
+            From Newton's Law of Universal Gravitation:
+            F = G * (m1 * m2) / r^2
+                => F = m1 * a1
+            m1 * a1 = G * (m1 * m2) / r^2
 
-
+            => a1 = G * m2 / r^2
 
             Where:
-            F_Mag = Magnitude of the Force
-            F_Dir = Direction of the Force
-            G = Gravitational Constant
-            m1, m2 = Masses of the bodies
-            r = Distance between the centers of the bodies
-            r1 = Position of the first body
-            r2 = Position of the second body
+                G = Gravitational Constant
+                a1 = Acceleration acting on the first body due to the second body
+                m2 = Mass of the second body
+                r = Distance between the two bodies
 
-            This gives the force applied by the First body on the Second body
+            For the direction of the acceleration, calculate the unit vector in the direction
+            of the second body from the first body and multiply it by the magnitude of the acceleration
         */
-        // Get the positions of the bodies
+        for (size_t i = 0; i < bodies.size(); i++) {
+            bodies[i]->setKinematicProperty("Acceleration", Math::Vector::Zero);
+            Math::Vector position = bodies[i]->getKinematicProperty("Position");
 
-        Math::Vector pos1 = body1->getKinematicProperty("Position");
-        Math::Vector pos2 = body2->getKinematicProperty("Position");
+            for (size_t j = 0; j < bodies.size(); j++) {
+                if (i == j) continue;
 
-        double mass1 = body1->getPhysicalProperty("Mass");
-        double mass2 = body2->getPhysicalProperty("Mass");
+                Math::Vector direction = bodies[j]->getKinematicProperty("Position") - position;
 
-        // Calculate the Direction of the Force
-        Math::Vector direction = pos2 - pos1;
+                double distance = Math::Operation::Length(direction);
+                direction = direction / distance;
 
-        // Get the lenght of the direction(Distance between the centers of the bodies)
-        double r = Math::Operation::Length(pos2 - pos1);
+                double mass = bodies[j]->getPhysicalProperty("Mass");
 
+                double accelerationMagnitude = Constants::GRAVITATIONAL_CONSTANT * mass / (distance * distance);
+                Math::Vector acceleration = direction * accelerationMagnitude;
 
-        // Normalise the direction
-        direction = Math::Operation::Normalize(direction);
-
-        // Calculate the Magnitude of the Force
-        double forceMagnitude = G * (mass1 * mass2) / (r * r);
-
-        // Calculate the Force
-        Math::Vector force = direction * forceMagnitude;
-
-        // Apply the force to the body
-        body2->addKinematicProperty("GForce", -force);
-        body1->addKinematicProperty("GForce", force);
+                bodies[i]->addKinematicProperty("Acceleration", acceleration);
+            }
+        }
     }
 
 } // namespace Physics
