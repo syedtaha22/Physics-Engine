0c833cce128b7cdbadd22484fdae61ab28e2664c
Syed Taha <143250416+syedtaha22@users.noreply.github.com>
2026-01-02 16:04:23 +0500

    Fix formatting of constants in Constants.hpp

--- Engine/Physics/headers/Constants.hpp
+++ Engine/Physics/headers/Constants.hpp
@@ -7,9 +7,9 @@ namespace Physics {
     public:
         // Orbital Mechanics
         static constexpr double GRAVITATIONAL_CONSTANT = 6.67430e-11;  // Gravitational constant, m^3 kg^-1 s^-2
-        static constexpr double STANDARD_GRAVITY = 9.80665;      // Standard gravity, m/s^2
-        static constexpr double SPEED_OF_LIGHT = 299792458.0;  // Speed of light, m/s
-        static constexpr double ASTRONOMICAL_UNIT = 1.496e11;    // Astronomical Unit, 1AU = 1.496e11 m
+        static constexpr double STANDARD_GRAVITY       = 9.80665;      // Standard gravity, m/s^2
+        static constexpr double SPEED_OF_LIGHT 	       = 299792458.0;  // Speed of light, m/s
+        static constexpr double ASTRONOMICAL_UNIT      = 1.496e11;     // Astronomical Unit, 1AU = 1.496e11 m
     };
 } // namespace Physics
 
